f1996f0cdc1e76dae09c77eb0f8f0e116fac7e35
Ben Wallberg <wallberg@umd.edu>
2013-03-18 10:49:24 -0400

    take first stab at determining building from the computer name

--- wstrack.R
+++ wstrack.R
@@ -18,16 +18,29 @@ num <- function(num) {
   format(num, big.mark = ",", scientific = FALSE)
 }
 
+getBuilding <- function(name) {
+  # strip off leading 'libwk'
+  name = substring(name,6)
+  # strip off numbers and everything following
+  name = gsub('^([^0-9]*).*$','\\1',name)
+  # convert empty string to 'unknown'
+  name[name==''] <- 'unknown'
+  return(name)
+}
+
 # read input file
 printf('Reading input file...')
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 1) {
-  wst = read.csv(args[1])
+  wst_orig= read.csv(args[1])
 } else {
-  wst = read.csv("/dev/stdin")
+  wst_orig= read.csv("/dev/stdin")
 }
 
 printf('Processing data...')
+wst <- transform(wst_orig, computer_name=tolower(computer_name))
+wst <- subset(wst, substr(computer_name,1,5) == 'libwk')
+wst <- transform(wst, building=getBuilding(computer_name))
 attach(wst)
 login = subset(wst, status=='login')
 logout = subset(wst, status=='logout')
@@ -38,7 +51,8 @@ printf('Time Period: %s to %s', min(timestamp_vector), max(timestamp_vector))
 printf('')
 
 printf('Summary Statistics')
-printf('  Total Entries:         %11s', num(nrow(wst)))
+printf('  Total Entries:         %11s', num(nrow(wst_orig)))
+printf('  Accepted Entries:      %11s', num(nrow(wst)))
 printf('  Logins:                %11s', num(nrow(login)))
 printf('  Logouts:               %11s', num(nrow(logout)))
 printf('  Unique Regular Users:  %11s', num(length(unique(subset(wst,guest_flag=='f')$user_hash))))
