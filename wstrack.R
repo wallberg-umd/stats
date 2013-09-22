fc29ea5d8f8e68c72489ce62c397d71bd9a0c70d
Ben Wallberg <wallberg@umd.edu>
2013-09-22 17:11:08 -0400

    map workstation names to locations using regexes

--- wstrack.R
+++ wstrack.R
@@ -10,6 +10,8 @@
 #   timestamp
 #   user_hash
 
+library(stringr)
+
 printf <- function(...) {
   print(sprintf(...), quote = FALSE)
 }
@@ -18,14 +20,41 @@ num <- function(num) {
   format(num, big.mark = ",", scientific = FALSE)
 }
 
-getBuilding <- function(name) {
-  # strip off leading 'libwk'
-  name = substring(name,6)
-  # strip off numbers and everything following
-  name = gsub('^([^0-9]*).*$','\\1',name)
-  # convert empty string to 'unknown'
-  name[name==''] <- 'unknown'
-  return(name)
+# Get location of workstation using regex matching on the workstation name
+location.cache = new.env(hash=TRUE)
+
+getLocation <- function(location) {
+
+  for (i in seq_along(location)) {
+    name = location[i]
+    
+    cache.hit = FALSE
+    location.mapped = NULL
+    
+    if (exists(name, envir=location.cache)) {
+      # use the cached value
+      location.mapped <- location.cache[[name]]
+      cache.hit = TRUE
+      
+    } else if (str_detect(name, perl('^libwkmck1f\\d+.*$'))) {
+      location.mapped <- 'McKeldin Library 1st floor'
+    
+    } else if (str_detect(name, perl('^libwkm[abcd]2f\\d+.*$'))) {
+      location.mapped <- 'McKeldin Library 2nd floor'
+    
+    } else {
+      location.mapped <- 'Unknown Location'
+    }
+    
+    location[i] <- location.mapped
+    
+    # save the mapping in the cache
+    if (! cache.hit) {
+      location.cache[[name]] = location.mapped
+    }
+  }
+
+  return(location)
 }
 
 # read input file
@@ -41,7 +70,7 @@ printf('Processing data...')
 names(wst_orig) <- gsub("\\.", "_", tolower(names(wst_orig)))
 wst <- transform(wst_orig, computer_name=tolower(computer_name))
 wst <- subset(wst, substr(computer_name,1,5) == 'libwk')
-wst <- transform(wst, building=getBuilding(computer_name))
+wst <- transform(wst, location=getLocation(computer_name))
 wst <- transform(wst, timestamp=as.POSIXct(timestamp, "%Y-%m-%d %H:%M:%S", tz="UTC"))
 wst <- transform(wst, wday=weekdays(as.Date(timestamp)))
 attach(wst)
@@ -63,10 +92,10 @@ printf('  Unique Guest Users:    %11s', num(length(unique(subset(wst,guest_flag=
 printf('  Unique Computer Names: %11s', num(length(unique(computer_name))))
 printf('')
 
-printf('Logins by Building')
-logins.building <- tapply(login$building, login$building, FUN=length)
-for (row in rownames(logins.building)) {
-  printf('  %8s %10s',row,num(logins.building[row]))
+printf('Logins by Location')
+logins.location <- tapply(login$location, login$location, FUN=length)
+for (row in rownames(logins.location)) {
+  printf('  %8s %10s',row,num(logins.location[row]))
 }
 printf('')
 
@@ -85,3 +114,4 @@ for (row in c('Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Satur
 printf('')
 
 #print(wst)
+#write.table(wst,file="data/out.csv",sep=",",row.names=TRUE)
